f7f3caed2485bf4c96f9e000a9bc6b8ba19e4b51
Malte Knezevic <m.knezevic@telekom.de>
2019-12-05 15:17:04 +0100

    Code kürzer geschrieben

--- Thanos.java
+++ Thanos.java
@@ -4,9 +4,8 @@ public class Thanos {
 
     public static void main(String[] args) {
 
-	   int removed = 0;
 
-	   Hero widow = new Hero("Black Widow", 34);
+	/*   Hero widow = new Hero("Black Widow", 34);
 	   Hero captain = new Hero("Captain America", 100);
 	   Hero vision = new Hero("Vision", 3);
 	   Hero iron = new Hero("Iron Man", 48);
@@ -14,24 +13,26 @@ public class Thanos {
 	   Hero thor = new Hero("Thor", 1500);
 	   Hero hulk = new Hero("Hulk", 49);
 	   Hero doctor = new Hero("Doctor Strange", 42);
+	   */
 	   
 	    ArrayList<Hero> idols = new ArrayList<>();
-	    idols.add(widow);
-	    idols.add(captain);
-	    idols.add(vision);
-	    idols.add(iron);
-	    idols.add(scarlett);
-	    idols.add(thor);
-	    idols.add(hulk);
-	    idols.add(doctor);
+	    idols.add(new Hero ("Black Widow", 34));
+	    idols.add(new Hero ("Captain America", 100));
+	    idols.add(new Hero ("Vision", 3));
+	    idols.add(new Hero ("Iron Man", 48));
+	    idols.add(new Hero ("Scarlett Witch", 29));
+	    idols.add(new Hero ("Thor",1500));
+	    idols.add(new Hero ("Hulk",49));
+	    idols.add(new Hero ("Doctor Strange", 42));
 
 
-	   thor.setAge(1501);
+ 
+	   idols.get(5).setAge(1501);
 
 	   Collections.shuffle(idols);
 
 	   idols.subList(idols.size()/2,idols.size()).clear();
-	
+
 	   for(Hero hero : idols){
 	      System.out.println("Name des Helden: "+hero.getName()+". Das Alter des Helden: "+hero.getAge());
 	   } 
